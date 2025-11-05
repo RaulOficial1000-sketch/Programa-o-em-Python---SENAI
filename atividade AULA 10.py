adb0ff9a9e6a63343f73a83b85dcbeac72558383
RAUL <raulteixeira.profissonal1000@gmail.com>
2025-11-04 21:59:03 -0300

    Add files via upload

---
+++ atividade AULA 10.py
@@ -0,0 +1,24 @@
+contador = 0
+while contador <= 1000:
+     print("Número:", contador)
+     contador += 1
+
+
+
+
+pessoas = 10
+ 
+lista = []
+
+
+while pessoas > 0:
+     p = input('digete + nomes')
+     pessoas = pessoas - 1
+     lista.append(p)
+     print(lista)
+
+
+   
+
+
+
